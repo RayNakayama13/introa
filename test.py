9a37df24ec4ba473b11c16ab488080beb60df589
RayNakayama13 <ray.nakayama@gmail.com>
2020-09-17 13:52:47 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,29 @@
+with open('input.txt', 'r') as f:
+    a = [f.read()]
+
+class FizzBuzzExt():
+
+    def __init__(self):
+        list = [(int(x[0]), x[2:]) for x in a.split('\n')]
+            for n in len(list)
+                i = list[n][0]
+                s = list[n][1]
+        list.sort()
+        self.m = a.pop()
+        return
+
+
+    def FizzBuzzExt(i, s, m):
+        Output = []
+        for i, s in list:
+            while
+            if m%i  == 0:
+                 Output.append(s)
+            else:
+
+        print(Output)
+
+z = [(1,3),(2,5)]
+for n in len(z):
+    i = z[n][0]
+    s = z[n][1]
